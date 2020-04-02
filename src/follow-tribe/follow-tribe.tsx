935a4192960d718e2ba3dee72eb36fcd47cede0d
Clark Kozak <clarkkozak@protonmail.com>
2020-04-01 20:02:53 -0600

    [WIP] Following all memebers using promises
    Added
      - compare existing members the user may already be following
       - getUserFollowing works as a callback in handleGetCommunity
    
    Todo
      - handle block timing. Steem
      -  won't allow you to mass follow like this in one block.

--- src/follow-tribe/follow-tribe.tsx
+++ src/follow-tribe/follow-tribe.tsx
@@ -29,22 +29,45 @@ const FollowTribe: React.FC = () => {
   const [selectedMembers, setSelectedMembers] = useState(new Set(members))
   const [amount] = useState(1000)
 
-   const handleFollow: any = async (event: React.FormEvent<HTMLInputElement>) => {
-    event.preventDefault()
-    console.log('clicked follow')
-    let follower = username
-    let following = members[6] // index 5, 6 as tests
-    console.log({follower, following, privateKey})
+  const getUserFollowing: any = async () => {
+
+    const results = await fetch('https://api.steemit.com', {
+      method: 'POST',
+      body: `{"jsonrpc":"2.0", "method":"follow_api.get_following", "params":{"account":"${username}","start":null,"limit":100}, "id":1}`
+    })
+      .then(data => data.json())
+      .catch(err => {
+        setError(true)
+        console.log('There was an error when fetching user followers: ' + err)
+      })
+
+    if (results) {
+      const listOfUserFollowing = results.result.reduce((arr, current) => {
+        return [...arr, current.following]
+      }, [])
+
+      return listOfUserFollowing
+    } else {
+      setError(true)
+    }
+    console.log(results)
+  }
+
+  const handleFollow: any = async (event: React.FormEvent<HTMLInputElement>) => {
+  event.preventDefault()
+  console.log('clicked follow')
+  let follower = username
+  const followMemberPromises: any = members.map(async (following) => {
     let status = await client.call('follow_api', 'get_following', [
       follower,
       following,
       'blog',
-      1,
-    ]);
-
+    1,
+  ]);
+  
     console.log({ status });
-
-    var type
+    
+    let type
     if (status.length > 0 && status[0].following === following) {
       type = ''
     } else {
@@ -59,23 +82,30 @@ const FollowTribe: React.FC = () => {
         what: [type], //null value for unfollow, 'blog' for follow
       },
     ])
-
+    
     const data: any = {
       id: 'follow',
       json: json,
       required_auths: [],
       required_posting_auths: [follower],
-  }
+    }
 
-    client.broadcast.json(data, privateKey)
+    return new Promise (async (resolve, reject) => {
+      await client.broadcast.json(data, privateKey)
       .then(res => {
-        console.log('user follow result: ', res);
+        resolve(console.log('user follow result: ', res))
       })
-      .catch(err => console.log(err))
-
+      .catch(err => {
+        reject(console.log(err))
+      })
+    })
+  }
+  )
+  
+  console.log(followMemberPromises) 
   }
 
-  const handleGetMember = async () => {
+  const handleGetCommunity: any = async (callback: () => {}) => {
     const results = await fetch('https://api.steemit.com', {
       method: 'POST',
       body: '{"jsonrpc":"2.0", "method":"follow_api.get_following", "params":{"account":"tribesteemup","start":null,"limit":100}, "id":1}'
@@ -89,24 +119,22 @@ const FollowTribe: React.FC = () => {
     setLoading(false)
 
     if (results) {
-      const listOfMembers = results.result.reduce((arr, current) => {
+      const currentUserFollowers: any = await callback()
+      let listOfMembers: any = results.result.reduce((arr, current) => {
         return [...arr, current.following]
       }, [])
 
-      setMembers(listOfMembers)
-      setSelectedMembers(new Set(listOfMembers))
+      const listOfNewFollowers: any = listOfMembers.filter(newFollower => {
+        return !currentUserFollowers.includes(newFollower)
+      })
+
+      console.log({listOfNewFollowers, currentUserFollowers, listOfMembers})
+
+      setMembers(listOfNewFollowers)
+      setSelectedMembers(new Set(listOfNewFollowers))
     } else {
       setError(true)
     }
-  }
-
-  React.useEffect(() => {
-    handleGetMember()
-  }, [])
-
-  const handleSubmit: any = (event: React.FormEvent<HTMLInputElement>) => {
-    event.preventDefault()
-    console.log(selectedMembers)
   }
 
   const handleChange: any = (member: string, value: boolean) => {
@@ -120,6 +148,11 @@ const FollowTribe: React.FC = () => {
 
     setSelectedMembers(newSelectedMembers)
   }
+
+
+  React.useEffect(() => {
+    handleGetCommunity(getUserFollowing)
+  }, [])
 
   return (
     <>
